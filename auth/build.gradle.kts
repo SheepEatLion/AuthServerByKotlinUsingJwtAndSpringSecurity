a16c684b0f098be3fd512344987b571bb372e338
SheepEatLion <gowjr207@gmail.com>
2023-05-05 16:25:17 +0900

    build: add redis dependency

--- auth/build.gradle.kts
+++ auth/build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-
+	implementation ("org.springframework.boot:spring-boot-starter-data-redis")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 }
