cee15390eef48b9d48c64bed65e9403c7bf209ae
SheepEatLion <gowjr207@gmail.com>
2023-05-06 16:56:44 +0900

    feat: apply security to token provider

--- auth/src/main/kotlin/com/example/auth/util/TokenProvider.kt
+++ auth/src/main/kotlin/com/example/auth/util/TokenProvider.kt
@@ -3,11 +3,11 @@ package com.example.auth.util
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import com.example.auth.model.entity.Member
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.InitializingBean
 import org.springframework.beans.factory.annotation.Value
-import java.security.Key
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
 import java.util.*
+import java.util.stream.Collectors
 
 class TokenProvider(
     @param:Value("\${jwt.secret}") private val SECRET: String,
@@ -17,21 +17,23 @@ class TokenProvider(
     private val accessTokenValidityMilliseconds: Long = ACCESS_TOKEN_VALIDITY * 1000000
     private val refreshTokenValidityMilliseconds: Long = REFRESH_TOKEN_VALIDITY * 1000000
 
-    fun generateAccessToken(member: Member): String {
+    fun generateAccessToken(authentication: Authentication): String {
+        val authorities = authentication.authorities.stream()
+            .map { obj: GrantedAuthority -> obj.authority }
+            .collect(Collectors.joining(","))
+
         return JWT.create()
-            .withSubject(member.name)
+            .withSubject(authentication.name)
             .withExpiresAt(Date(Date().time + accessTokenValidityMilliseconds))
-            .withClaim("id", member.id)
             .withClaim("auth", authorities)
             .sign(Algorithm.HMAC512(SECRET))
     }
 
-    fun generateRefreshToken(member: Member, accessToken: String): String {
+    fun generateRefreshToken(authentication: Authentication, accessToken: String): String {
         return JWT.create()
-            .withSubject()
+            .withSubject(authentication.name)
             .withExpiresAt(Date(Date().time + refreshTokenValidityMilliseconds))
             .withClaim("accessToken", accessToken)
-            .withClaim("auth", authorities)
             .sign(Algorithm.HMAC512(SECRET))
     }
 }
