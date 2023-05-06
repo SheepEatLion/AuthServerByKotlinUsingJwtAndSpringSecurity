50396abbacbf060d3b1c2c7f1ae294d5861a49f2
SheepEatLion <gowjr207@gmail.com>
2023-05-06 15:42:51 +0900

    feat: make member domain & entity

--- auth/src/main/kotlin/com/example/auth/domain/Member.kt
+++ auth/src/main/kotlin/com/example/auth/domain/Member.kt
@@ -1,4 +1,25 @@
 package com.example.auth.domain
 
-class Member {
-}
+import jakarta.persistence.*
+
+@Entity
+@Table
+class Member(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    @Column(length = 50)
+    val name: String,
+
+    @Column(length = 50)
+    val email: String? = null,
+
+    @ManyToMany
+    @JoinTable(
+        name = "member_authority",
+        joinColumns = [JoinColumn(name = "member_id", referencedColumnName = "id")],
+        inverseJoinColumns = [JoinColumn(name = "authority_name", referencedColumnName = "authority_name")]
+    )
+    val authorities: Set<Authority>,
+)
