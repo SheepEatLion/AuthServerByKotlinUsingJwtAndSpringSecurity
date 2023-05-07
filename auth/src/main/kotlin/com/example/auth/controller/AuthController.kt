4a38c6088433f6d13baa227329d5745d8eddf76d
장준영 <gowjr207@gmail.com>
2023-05-07 18:29:19 +0900

    feat: token generate API

--- auth/src/main/kotlin/com/example/auth/controller/AuthController.kt
+++ auth/src/main/kotlin/com/example/auth/controller/AuthController.kt
@@ -1,16 +1,32 @@
 package com.example.auth.controller
 
+import com.example.auth.model.dto.AccessTokenResponse
+import com.example.auth.model.dto.GenerateTokenRequest
+import com.example.auth.service.AuthService
+import jakarta.validation.Valid
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("/auths")
+@RequestMapping("v1/auth")
 @RestController
 class AuthController(
-
+    val authService: AuthService,
 ) {
-    @PostMapping
-    fun test() {
+    @PostMapping("/token/generate")
+    fun generate(@RequestBody @Valid generateTokenRequest: GenerateTokenRequest): ResponseEntity<AccessTokenResponse> {
+        return authService.generate(generateTokenRequest)
+    }
+
+    @PostMapping("/token/invalidate")
+    fun invalidate() {
+
+    }
+
+    @PostMapping("/token/refresh")
+    fun refresh() {
 
     }
 }
--- auth/src/main/kotlin/com/example/auth/service/AuthService.kt
+++ auth/src/main/kotlin/com/example/auth/service/AuthService.kt
@@ -1,8 +1,42 @@
 package com.example.auth.service
 
+import com.example.auth.model.dto.AccessTokenResponse
+import com.example.auth.model.dto.GenerateTokenRequest
+import com.example.auth.util.TokenProvider
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Service
 
 @Service
-class AuthService {
+class AuthService(
+    val tokenProvider: TokenProvider,
+    val authenticationManagerBuilder: AuthenticationManagerBuilder,
+) {
+    fun generate(generateTokenRequest: GenerateTokenRequest): ResponseEntity<AccessTokenResponse> {
+        val authenticationToken = UsernamePasswordAuthenticationToken(generateTokenRequest.memberId, generateTokenRequest.memberEmail)
+        val authentication: Authentication = authenticationManagerBuilder.`object`.authenticate(authenticationToken)
 
+        SecurityContextHolder.getContext().authentication = authentication
+
+        val accessToken: String = tokenProvider.generateAccessToken(authentication = authentication)
+        val refreshToken: String = tokenProvider.generateRefreshToken(authentication = authentication, accessToken = accessToken)
+
+        val httpHeaders = HttpHeaders()
+        httpHeaders.add("refresh-token", refreshToken)
+
+        return ResponseEntity<AccessTokenResponse>(AccessTokenResponse(accessToken), httpHeaders, HttpStatus.OK)
+    }
+
+    fun invalidate() {
+
+    }
+
+    fun refresh() {
+
+    }
 }
