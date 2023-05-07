3a25e603ce6a7fb6f5968efeff95affc221cbd86
SheepEatLion <gowjr207@gmail.com>
2023-05-07 15:48:49 +0900

    Revert "build: remove h2 dependency"
    This reverts commit 7bcf9ad2

--- auth/build.gradle.kts
+++ auth/build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation ("org.springframework.boot:spring-boot-starter-data-redis")
+	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 }
